09d899cb4135f31d2bafc6958d4d99a75e150dbe
Misha Kushka <kushka.misha@gmail.com>
2023-10-04 14:37:26 -0700

    Add function comments to ProofsHelper

--- contracts/libs/ProofsHelper.sol
+++ contracts/libs/ProofsHelper.sol
@@ -5,6 +5,10 @@ import { ProofTypes } from './common/ProofTypes.sol';
 import { StringsExpanded } from './StringsExpanded.sol';
 import { IProofsMetadata } from '../interfaces/IProofsMetadata.sol';
 
+/**
+ * ProofsHelper library helps Proofs smart contract to generate Proof-of-Authority, Proof-of-Signature, and
+ * Proof-of-Agreement in a text format
+ */
 library ProofsHelper {
     using StringsExpanded for string;
     using StringsExpanded for bytes;
@@ -12,17 +16,21 @@ library ProofsHelper {
     using StringsExpanded for uint256;
 
     /**
-     * Get full Proof-of-Authority or Proof-of-Signature
+     * Returns full Proof-of-Authority or Proof-of-Signature (data with signature)
+     * @param _creator Creator of the proof
+     * @param _signature Signature of the proof data
+     * @param _data Data that the user have signed
+     * @return _proof The proof object as a string
      */
     function getProofOfAuthorityOrSignature(
         address _creator,
         bytes calldata _signature,
         string memory _data
-    ) external pure returns (string memory proof) {
+    ) external pure returns (string memory _proof) {
         require(_creator != address(0), 'No creator');
         require(_signature.length > 0, 'No signature');
         require(_data.length() > 0, 'No data');
-        proof = string(
+        _proof = string(
             abi.encodePacked(
                 '{"address":"',
                 _creator.toString(),
@@ -35,6 +43,16 @@ library ProofsHelper {
         );
     }
 
+    /**
+     * Returns Proof-of-Authority data for the creator to sign
+     * @param _proofsMetadata EIP712 part of the proof data defined in ProofsMetadata smart contract
+     * @param _creator Creator of the agreement
+     * @param _signers Signers of the agreement
+     * @param _agreementFileCID IPFS CID of the agreement file
+     * @param _version EIP712 version of the data
+     * @param _timestamp Timestamp of the proof
+     * @return data Proof-of-Authority data
+     */
     function getProofOfAuthorityData(
         address _proofsMetadata,
         address _creator,
@@ -67,6 +85,15 @@ library ProofsHelper {
             );
     }
 
+    /**
+     * Returns Proof-of-Signature data for the signer to sign
+     * @param _proofsMetadata EIP712 part of the proof data defined in ProofsMetadata smart contract
+     * @param _signer Current signer of the agreement from the list of agreement signers
+     * @param _proofOfAuthorityCID IPFS CID of the Proof-of-Authority
+     * @param _version EIP712 version of the data
+     * @param _timestamp Timestamp of the proof
+     * @return data Proof-of-Signature data
+     */
     function getProofOfSignatureData(
         address _proofsMetadata,
         address _signer,
@@ -92,6 +119,14 @@ library ProofsHelper {
             );
     }
 
+    /**
+     * Returns Proof-of-Agreement data that is equal to Proof-of-Agreement as it requires no
+     * signature
+     * @param _proofOfAuthorityCID IPFS CID of the Proof-of-Authority
+     * @param _proofsOfSignatureCID Array of IPFS CID of every Proof-of-Signature from the agreement
+     * @param _timestamp Timestamp of the proof
+     * @return data Proof-of-Agreement data
+     */
     function getProofOfAgreementData(
         string calldata _proofOfAuthorityCID,
         string[] calldata _proofsOfSignatureCID,
@@ -130,13 +165,21 @@ library ProofsHelper {
             );
     }
 
+    /**
+     * Returns the core message (without EIP712 metadata) of Proof-of-Authority for the creator to
+     * sign
+     * @param _creator Agreement creator address
+     * @param _signers Array of signers of the agreement
+     * @param _agreementFileCID IPFS CID of the agreement file
+     * @return _message Proof-of-Authority message to sign
+     */
     function getProofOfAuthorityDataMessage(
         address _creator,
         address[] calldata _signers,
         string calldata _agreementFileCID,
         uint256 _timestamp
-    ) public pure returns (string memory message) {
-        message = string(
+    ) public pure returns (string memory _message) {
+        _message = string(
             abi.encodePacked(
                 '{"from":"',
                 _creator.toString(),
@@ -151,12 +194,20 @@ library ProofsHelper {
         );
     }
 
+    /**
+     * Returns the core message (without EIP712 metadata) of Proof-of-Signature for the signer to
+     * sign
+     * @param _signer Current signer of the agreement from the list of agreement signers
+     * @param _proofOfAuthorityCID IPFS CID of the Proof-of-Authority
+     * @param _timestamp Timestamp of the proof
+     * @return _message Proof-of-Authority message to sign
+     */
     function getProofOfSignatureDataMessage(
         address _signer,
         string calldata _proofOfAuthorityCID,
         uint256 _timestamp
-    ) public pure returns (string memory message) {
-        message = string(
+    ) public pure returns (string memory _message) {
+        _message = string(
             abi.encodePacked(
                 '{"signer":"',
                 _signer.toString(),
@@ -169,6 +220,11 @@ library ProofsHelper {
         );
     }
 
+    /**
+     * Generates a JSON stringified content with signers of the agreement
+     * @param _signers Array of signers of the agreement
+     * @return res JSON stringified list of signers
+     */
     function generateSignersJSON(address[] calldata _signers) public pure returns (string memory) {
         string memory res = '[';
 
